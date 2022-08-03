a545cc2c7f35c2b403efdee7ec60fcd5adef2da4
LNSHRIVAS <76100357+LNSHRIVAS@users.noreply.github.com>
2022-08-03 14:10:34 +0530

    gray slicing image

---
+++ Operations on image/processing.m
@@ -0,0 +1,20 @@
+clc
+clear all
+close all
+a=imread('https://images.pexels.com/photos/792381/pexels-photo-792381.jpeg?cs=srgb&dl=pexels-george-desipris-792381.jpg&fm=jpg');
+imshow(a),title('original image')
+%using 128 gray levels
+figure,imshow(grayslice(a,128),gray(128)),
+title('Image with 128 gray level')
+%using 64 gray levels
+figure,imshow(grayslice(a,64),gray(64)),
+title('Image with 64 gray level')
+%using 32 gray levels
+figure,imshow(grayslice(a,32),gray(32)),
+title('Image with 32 gray level')
+%using 16 gray levels
+figure,imshow(grayslice(a,16),gray(16)),
+title('Image with 16 gray level')
+%using 8 gray levels
+figure,imshow(grayslice(a,8),gray(8)),
+title('Image with 8 gray level')
