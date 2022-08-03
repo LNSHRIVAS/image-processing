41909ee14eaa2c5eac95832a001a6d4ea32a483a
LNSHRIVAS <76100357+LNSHRIVAS@users.noreply.github.com>
2022-08-03 13:07:07 +0530

    Delete histogram.m

--- histogram.m
+++
@@ -1,9 +0,0 @@
-clc
-clear all
-close all
-a=imread('https://unsplash.com/photos/G9bDsVeHM7I/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mnx8Z3JheXNjYWxlfGVufDB8fHx8MTY1OTQ0NzgzOQ&force=true');
-b=histeq(a);
-subplot(2,2,1),imshow(a),title('original image');
-subplot(2,2,2),imshow(b),title('After histogram equalisation');
-subplot(2,2,3),imhist(a),title('original histogram');
-subplot(2,2,4),imhist(b),title('After histogram equalisation');
